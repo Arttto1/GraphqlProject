3dbce270c3efd20a7c6bbf0297dcd2eb6a6082ba
arthur <arthurnunes10@live.com>
2024-12-12 02:42:24 -0300

    changed server logic to serverless to vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const colors = require("colors")
 const cors = require("cors")
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const connectDB = require("./config/db")
-const port = process.env.PORT || 5000;
 
 module.exports = async (req, res) => {
   const app = express();
